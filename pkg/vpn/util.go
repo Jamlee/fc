0cd7527143de62effd5a0f01b45f77630484c166
jamlee <1575103047@qq.com>
2019-03-26 01:50:06 +0800

    no need external tool anymore

--- pkg/vpn/util.go
+++ pkg/vpn/util.go
@@ -2,10 +2,8 @@ package vpn
 
 import (
 	"errors"
-	"log"
 	"math/rand"
 	"net"
-	"os/exec"
 	"time"
 )
 
@@ -20,16 +18,4 @@ func hostToIP(addr string) (net.IP, error) {
 
 	rand.Seed(time.Now().Unix())
 	return addrs[rand.Int()%len(addrs)], nil
-}
-
-func commandExec(command string, args []string, debug bool) error {
-	cmd := exec.Command(command, args...)
-	if debug {
-		log.Println("exec "+command+": ", args)
-	}
-	e := cmd.Run()
-	if e != nil {
-		log.Println("Command failed: ", e)
-	}
-	return e
 }
